c11e3133578fed7d7e1ed55b088a292aff23f96b
ITAYCO890 <59862259+ITAYCO890@users.noreply.github.com>
2023-02-21 09:35:37 +0200

    Initialize testing/custom-schema-and-test

---
+++ tests/generic/test_is_even.sql
@@ -0,0 +1,31 @@
+{% test is_even(model, column_name, bool) %}
+
+{% do log(model, True) %}
+{% do log(model.database, True) %}
+{% do log(model.schema, True) %}
+{% do log(model.identifier, True) %}
+
+with validation as (
+
+    select
+        {{ column_name }} as even_field
+
+    from {{ model }}
+
+),
+
+validation_errors as (
+
+    select
+        even_field
+
+    from validation
+    -- if this is true, then even_field is actually odd!
+    where (even_field % 2) = 1 and {{ bool }}
+
+)
+
+select *
+from validation_errors
+
+{% endtest %}
